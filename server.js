c31571d3413c9e57023dfa49bea5e004fde98da1
Mohd Azlan Hashim <78134294+lanzslumber85@users.noreply.github.com>
2021-04-15 10:58:32 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
+const users = require("./routes/api/users");
 const app = express();
 
 // Bodyparser middleware
@@ -16,6 +18,15 @@ mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("MongoDB successfully connected!"))
     .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 // Set the port for our server to run on and have our app listen on this port
 const port = process.env.PORT || 5000;
