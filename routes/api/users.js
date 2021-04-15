52d53b168b37daad602f918b0371fe60d9573d46
Mohd Azlan Hashim <78134294+lanzslumber85@users.noreply.github.com>
2021-04-15 08:07:28 +0800

    Create Register endpoint

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,3 +10,31 @@ const validateLoginInput = require("../../validation/login");
 
 // Load User model
 const User = require("../../models/User");
+
+router.post("/register", (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if (!isValid) return res.status(400).json(errors);
+
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user)
+            return res.status(400).json({ email: "Email already exists!" });
+
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                newUser.password = hash;
+                newUser
+                    .save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err));
+            });
+        });
+    });
+});
